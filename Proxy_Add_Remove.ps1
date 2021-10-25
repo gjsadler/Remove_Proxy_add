12084fd18169623ea01a63c420ba88392471c450
gjsadler <50763466+gjsadler@users.noreply.github.com>
2021-10-25 13:03:58 -0500

    Add files via upload

---
+++ Proxy_Add_Remove.ps1
@@ -0,0 +1,23 @@
+﻿#Grabs CSV file
+$filePath = Read-Host "Enter CSV filepath"
+$ADUsers = Import-csv $filePath
+Write-Host "Importing $filePath"
+Start-Sleep -Seconds 2
+Import-Module activedirectory
+
+$counter = 0
+Start-Sleep -Seconds 2
+
+#creates var for each column 
+foreach ($user in $ADUsers) {
+
+$Username = $User.username
+$proxyAddresses = "smtp:$Username@domain.com"
+
+
+Set-ADUser -Identity $Username -Remove @{ProxyAddresses= $proxyAddresses} 
+
+}
+
+
+	
